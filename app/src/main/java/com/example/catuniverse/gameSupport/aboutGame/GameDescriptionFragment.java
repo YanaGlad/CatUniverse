7177c745f3c5eb9d9bf823d7f07e264da9d96bbc
YanaGlad <olesya_leader@mail.ru>
2021-05-07 13:48:45 +0300

    button disappearing

--- app/src/main/java/com/example/catuniverse/gameSupport/aboutGame/GameDescriptionFragment.java
+++ app/src/main/java/com/example/catuniverse/gameSupport/aboutGame/GameDescriptionFragment.java
@@ -8,10 +8,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.catuniverse.R;
 import com.example.catuniverse.gameSupport.databaseHelpers.Cat;
 
@@ -24,8 +26,8 @@ public class GameDescriptionFragment extends Fragment {
     private TextView title, description;
     private ImageView imageView;
     private RecyclerView recyclerView;
-    private Button rooms, cats, time, strategy, maths, credits;
-    private ArrayList<Cat>nothing;
+    private Button rooms, cats, time, strategy, maths, credits, sortAttack, sortDelay, sortPrice;
+    private ArrayList<Cat> nothing;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -42,8 +44,13 @@ public class GameDescriptionFragment extends Fragment {
         maths = view.findViewById(R.id.mathsButton);
         credits = view.findViewById(R.id.creditsButton);
         recyclerView = view.findViewById(R.id.recycle);
+        sortAttack = view.findViewById(R.id.sortAttack);
+        sortDelay = view.findViewById(R.id.sortDelay);
+        sortPrice = view.findViewById(R.id.sortPrice);
 
-
+        sortAttack.setVisibility(View.GONE);
+        sortDelay.setVisibility(View.GONE);
+        sortPrice.setVisibility(View.GONE);
 
         final Context context = getContext();
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
@@ -52,49 +59,80 @@ public class GameDescriptionFragment extends Fragment {
         nothing = new ArrayList<>();
 
         rooms.setOnClickListener((vie) -> {
-                recyclerView.setAdapter(new CatAdapter(nothing, context));
-                title.setText(rooms.getText());
-                description.setText(R.string.rooms_description);
-                imageView.setImageResource(R.drawable.roommoon);
+
+            sortAttack.setVisibility(View.GONE);
+            sortDelay.setVisibility(View.GONE);
+            sortPrice.setVisibility(View.GONE);
+
+            recyclerView.setAdapter(new CatAdapter(nothing, context));
+            title.setText(rooms.getText());
+            description.setText(R.string.rooms_description);
+            imageView.setImageResource(R.drawable.roommoon);
         });
 
         cats.setOnClickListener((vie) -> {
-                title.setText(cats.getText());
-                description.setText(R.string.cats_description); //"There you can see cats you already have." +
-                recyclerView.setAdapter(new CatAdapter(listOfCats, context));
-                imageView.setImageResource(R.drawable.room);
+
+            sortAttack.setVisibility(View.VISIBLE);
+            sortDelay.setVisibility(View.VISIBLE);
+            sortPrice.setVisibility(View.VISIBLE);
+
+            title.setText(cats.getText());
+            description.setText(R.string.cats_description); //"There you can see cats you already have." +
+            recyclerView.setAdapter(new CatAdapter(listOfCats, context));
+            //btn.setVisible
+            imageView.setImageResource(R.drawable.room);
         });
 
 
         time.setOnClickListener((vie) -> {
-                recyclerView.setAdapter(new CatAdapter(nothing, context));
-                title.setText(time.getText());
-                description.setText(R.string.time_description);
-                imageView.setImageResource(R.drawable.time);
+
+            sortAttack.setVisibility(View.GONE);
+            sortDelay.setVisibility(View.GONE);
+            sortPrice.setVisibility(View.GONE);
+
+            recyclerView.setAdapter(new CatAdapter(nothing, context));
+            title.setText(time.getText());
+            description.setText(R.string.time_description);
+            imageView.setImageResource(R.drawable.time);
         });
 
         strategy.setOnClickListener((vie) -> {
-                recyclerView.setAdapter(new CatAdapter(nothing, context));
-                title.setText(strategy.getText());
-                description.setText(R.string.strategy_description);
-                imageView.setImageResource(R.drawable.strategy);
 
-            }
+                    sortAttack.setVisibility(View.GONE);
+                    sortDelay.setVisibility(View.GONE);
+                    sortPrice.setVisibility(View.GONE);
+
+                    recyclerView.setAdapter(new CatAdapter(nothing, context));
+                    title.setText(strategy.getText());
+                    description.setText(R.string.strategy_description);
+                    imageView.setImageResource(R.drawable.strategy);
+
+                }
         );
 
         maths.setOnClickListener((vie) -> {
-                recyclerView.setAdapter(new CatAdapter(nothing, context));
-                title.setText(maths.getText());
-                description.setText(R.string.maths_description);
-                imageView.setImageResource(R.drawable.maths);
-            }
+
+                    sortAttack.setVisibility(View.GONE);
+                    sortDelay.setVisibility(View.GONE);
+                    sortPrice.setVisibility(View.GONE);
+
+                    recyclerView.setAdapter(new CatAdapter(nothing, context));
+                    title.setText(maths.getText());
+                    description.setText(R.string.maths_description);
+                    imageView.setImageResource(R.drawable.maths);
+                }
         );
 
         credits.setOnClickListener((vie) -> {
-                recyclerView.setAdapter(new CatAdapter(nothing, context));
-                title.setText(credits.getText());
-                description.setText("Game Developer : Yana Gladkikh \n" + "Supported by Samsung IT School \n");
-                imageView.setImageResource(R.drawable.samsungitschool);
+
+            sortAttack.setVisibility(View.GONE);
+            sortDelay.setVisibility(View.GONE);
+            sortPrice.setVisibility(View.GONE);
+
+            recyclerView.setAdapter(new CatAdapter(nothing, context));
+            title.setText(credits.getText());
+            description.setText("Game Developer : Yana Gladkikh \n" + "Supported by Samsung IT School \n");
+            imageView.setImageResource(R.drawable.samsungitschool);
         });
 
         return view;
