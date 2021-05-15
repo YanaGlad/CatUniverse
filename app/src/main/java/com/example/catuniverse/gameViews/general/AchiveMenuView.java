e073840c33753bcb20218f5cdb268890704afe72
YanaGlad <olesya_leader@mail.ru>
2021-05-15 21:32:40 +0300

    achievements design improved

--- app/src/main/java/com/example/catuniverse/gameViews/general/AchiveMenuView.java
+++ app/src/main/java/com/example/catuniverse/gameViews/general/AchiveMenuView.java
@@ -15,22 +15,23 @@ import java.util.ArrayList;
 public class AchiveMenuView extends GameView {
 
     private ArrayList<BasicButton> basicButtons;
-    private ArrayList<Bitmap>achievementIcons;
+    private ArrayList<Bitmap> achievementIcons;
 
     public AchiveMenuView(MainRunActivity mainRunActivity) {
         super(mainRunActivity);
 
         basicButtons = new ArrayList<>();
         achievementIcons = new ArrayList<>();
+        achievementIcons.add(BitmapLoader.strengthAch);
 
-        int x = 0, y = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 3; j++) {
-                basicButtons.add(new BasicButton(getMainRunActivity(), x, y, BitmapLoader.grayIcon, BitmapLoader.grayIcon, false));
-                x += 100;
-            }
-            x = 0;
-            y += 100;
+        int x = 0, y = 100;
+        for (int i = 0; i < achievementIcons.size(); i++) {
+            //for (int j = 0; j < achievementIcons.size(); j++) {
+            basicButtons.add(new BasicButton(getMainRunActivity(), x, y, achievementIcons.get(i), achievementIcons.get(i), false));
+            x += 100;
+            //  }
+            //  x = 0;
+            //   y += 100;
         }
     }
 
@@ -38,6 +39,9 @@ public class AchiveMenuView extends GameView {
     public void run() {
         repaint();
         BasicGameSupport.drawGrid(getGamePaint());
+        getGamePaint().setVisibleBitmap(BitmapLoader.longBlueRect, -20, -198);
+        super.getGamePaint().write(super.getMainRunActivity().getString(R.string.achievements), 230, 50, Color.WHITE, 45);
+
         for (BasicButton btn : basicButtons) {
             btn.run(getGamePaint());
         }
