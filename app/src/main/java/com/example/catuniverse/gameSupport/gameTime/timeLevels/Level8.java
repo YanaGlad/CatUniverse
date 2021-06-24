6e32ceab91f8cd005b43f98f3facf4dfc6b0b9b7
YanaGlad <olesya_leader@mail.ru>
2021-06-24 21:16:18 +0300

    lvl 8

--- app/src/main/java/com/example/catuniverse/gameSupport/gameTime/timeLevels/Level8.java
+++ app/src/main/java/com/example/catuniverse/gameSupport/gameTime/timeLevels/Level8.java
@@ -25,31 +25,15 @@ public class Level8 extends TimeLevel {
         this.mainRunActivity = mainRunActivity;
         gameOver = false;
 
-        timeTallPlatformArrayList.add(new TimeTallPlatform(2050, 520));
-        timeTallPlatformArrayList.add(new TimeTallPlatform(2050, 900));
 
-        passingDoor = new BasicButton(mainRunActivity, 2070, -440, blueDoor, blueDoorOpened, true);
+        passingDoor = new BasicButton(mainRunActivity, 110, 430, blueDoor, blueDoorOpened, true);
 
-        gameItems.add(new TimeDecoration(2150, -800, blueDecorStation, true));
-
-        gameItems.add(passingDoor);
-        int xO = 300;
-        int yO = 500;
-        for (int i = 0; i < 10; i++) {
-            gameItems.add(new TimePlatform(xO, yO));
-            xO += 170;
-            yO -= 90;
-        }
     }
 
     @Override
     public void run(GamePaint gamePaint) {
         super.run(gamePaint);
         repaint();
-//        System.out.println("DOO");
-        for (GameItem b : gameItems) b.run(gamePaint);
-        passingDoor.repaint();
-        for (TimeTallPlatform tb : timeTallPlatformArrayList) tb.run(gamePaint);
         passingDoor.repaint();
         super.endingRun(gamePaint, mainRunActivity);
     }
@@ -58,9 +42,7 @@ public class Level8 extends TimeLevel {
     public void repaint() {
         super.repaint();
         passingDoor.repaint();
-        super.tallPlatformRepaint();
-        passingDoor.repaint();
-    }
+     }
 
     @Override
     public void repaint(double speed, double jumSpeed) {
