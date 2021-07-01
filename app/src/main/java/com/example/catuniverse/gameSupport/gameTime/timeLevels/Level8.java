f6207ef1bf2c9104531d61b9b1d6da52d96d0704
YanaGlad <olesya_leader@mail.ru>
2021-07-01 22:19:34 +0300

    go back

--- app/src/main/java/com/example/catuniverse/gameSupport/gameTime/timeLevels/Level8.java
+++ app/src/main/java/com/example/catuniverse/gameSupport/gameTime/timeLevels/Level8.java
@@ -13,6 +13,7 @@ import static com.example.catuniverse.gameSupport.BitmapLoader.blueDecorStation;
 import static com.example.catuniverse.gameSupport.BitmapLoader.blueDoor;
 import static com.example.catuniverse.gameSupport.BitmapLoader.blueDoorOpened;
 import static com.example.catuniverse.gameSupport.BitmapLoader.blueGround;
+import static com.example.catuniverse.gameSupport.BitmapLoader.bluePlatform;
 import static com.example.catuniverse.gameSupport.BitmapLoader.electrodynamixMusic;
 import static com.example.catuniverse.gameSupport.BitmapLoader.movingSpaceBackground;
 
@@ -26,6 +27,14 @@ public class Level8 extends TimeLevel {
         gameOver = false;
 
 
+        int yY = 550;
+        int xX = 700;
+        for (int i = 0; i < 20; i++) {
+            gameItems.add(new TimePlatform(xX, yY, bluePlatform));
+            xX += 100;
+            yY -= 70;
+        }
+
         passingDoor = new BasicButton(mainRunActivity, 110, 430, blueDoor, blueDoorOpened, true);
 
     }
@@ -35,6 +44,7 @@ public class Level8 extends TimeLevel {
         super.run(gamePaint);
         repaint();
         passingDoor.repaint();
+        for (GameItem b : gameItems) b.run(gamePaint);
         super.endingRun(gamePaint, mainRunActivity);
     }
 
